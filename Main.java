11db90a077864d154471316dcea4f247d7645f3a
VijayNamboodiri <vijay.namboodiri@hgs.hiddengeniusproject.com>
2022-01-13 02:48:23 +0000

    variables about me

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+     String name = new String("Vijay");
+  String dreamjob = new String("Comedian");
+  String age = new String("15 years old");
+  System.out.println("My name is  "+ name + " I am " + age + " And my dream job is being a " + dreamjob);
+
+  }
+}
