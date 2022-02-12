61bc9fd43cc502e62f75fc5e761ccdde895e53d6
VijayNamboodiri <85519890+VijayNamboodiri@users.noreply.github.com>
2022-02-12 12:26:47 -0800

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    string name = "Vijay";
+    print("Hello, " + "my name is " + name + ". ");
+    string teamates = "Jonathan, Quincy, Simon, and Isaac";
+    print("My teamates are, " + teamates + ". ");
+    string Message = "I look forward to working with and making new memories with ";
+    print(Message + teamates + " going forward. ");  
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
